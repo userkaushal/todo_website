29a3ef98f86ac80245b26e413264b4a9ac0d3c64
userkaushal <kaushamahida@gmail.com>
2024-07-05 22:46:01 +0530

    comments

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,10 @@ const Navbar = () => {
   return (
     <nav className='flex justify-between md:justify-around bg-stone-600 text-white p-2 sm:justify-between mb-5'>
         <span className='font-bold text-2xl'>Todos</span>
-        <ul className='flex gap-6 font-semibold text-xl'>
+        {/* <ul className='flex gap-6 font-semibold text-xl'>
             <li className='cursor-pointer'>Home</li>
             <li className='cursor-pointer'>Tasks</li>
-        </ul>
+        </ul> */}
     </nav>
   )
 }
